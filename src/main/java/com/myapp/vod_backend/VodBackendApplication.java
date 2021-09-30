a6632c21d71f95463f82ed8a88aafae7447c0168
Paweł Piętoń <pawel.pieton@gmail.com>
2021-09-30 18:15:07 +0200

    First commit

---
+++ src/main/java/com/myapp/vod_backend/VodBackendApplication.java
@@ -0,0 +1,13 @@
+package com.myapp.vod_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VodBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VodBackendApplication.class, args);
+    }
+
+}
